8b872d09297ac950e4e958ac3fd0199c5b97ceda
Gobinda Malakar <gobindamalakar68@gmail.com>
2023-04-21 01:03:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+	    let str = document.getElementById("rope-lengths").value;
+	    let arr = str.split(",");
+	    let n = arr.length;
+        let pq = new MinHeap(n);
+        for(let i = 0; i < n; i++) {
+            pq.insertKey(arr[i]);
+        }
+        pq.MinHeapify(0);
+        let res = 0;
+    
+        //using a loop while there is more than one element in priority queue.
+        while (pq.heap_size > 1) 
+        {
+            //storing the first and second numbers from priority queue.
+            let first = pq.extractMin();
+            
+            let second = pq.extractMin();
+         
+    
+            //adding their sum in result.
+            res += first + second;
+            
+            //pushing the sum of first and second numbers in priority queue.
+            pq.insertKey(first + second);
+        }
+        //returning the result.
+	    document.getElementById("result").innerHTML = res;
+        return res;
 }  
